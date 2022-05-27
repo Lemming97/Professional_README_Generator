6809e15e5e6efe8ff3d80702edd42221298be0fe
Lindsey Martin <martinlindsey87@yahoo.com>
2022-05-26 22:46:00 -0700

    Updated questions

--- README.md
+++ README.md
@@ -10,19 +10,28 @@ THEN a high-quality, professional README.md is generated with the title of my pr
 WHEN I enter my project title
 THEN this is displayed as the title of the README
 
-WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+WHEN I enter a 
+1.description, 
+2. installation instructions, 
+3. usage information, 
+4. contribution guidelines, and 
+5. test instructions
+THEN this information is added to the sections of the README entitled 
+1.Description, 
+2. Installation, 
+3. Usage, 
+4. Contributing, and 
+5. Tests
 
 
 WHEN I choose a license for my application from a list of options
 THEN a badge for that license is added near the top of the README and a notice is added to the section of the 
-
 README entitled License that explains which license the application is covered under
+
 WHEN I enter my GitHub username
-
 THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-WHEN I enter my email address
 
+WHEN I enter my email address
 THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
 
 WHEN I click on the links in the Table of Contents
--- index.js
+++ index.js
@@ -2,24 +2,76 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+//Connect the generateMarkdown script  
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [{
         type: 'input',
-        name: 'name',
-        message: 'What is your name?'
+        name: 'title',
+        message: 'What is the title of your project?'
     },
     {
-        type: 'checkbox',
-        message: 'What languages do you know?',
-        name: 'stack',
-        choices: ['HTML', 'CSS', 'JavaScript', 'MySQL']
+        pe: 'input',
+        message: 'Please enter a one-sentence description of your project:',
+        name: 'description',
+    },
+    {
+        pe: 'input',
+        message: 'Please tell us about your project:',
+        name: 'about',
+    },
+    {
+        pe: 'input',
+        message: 'What are the installation instructions for your project?',
+        name: 'installation',
+    },
+    {
+        pe: 'input',
+        name: 'usage',
+        message: 'What will this project be used for?',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Are there any contribution guidelines?',
+    },
+    {
+        type: 'input',
+        message: 'Please enter any testing protocols you used for this project?',
+        name: 'test',
     },
     {
         type: 'list',
-        message: 'What is your preferred method of communication?',
-        name: 'contact',
-        choices: ['email', 'phone', 'telekinesis']
+        message: 'Please select the license you used for this project:',
+        name: 'license',
+        choices: ['Apache 2.0', 'MIT', 'Mozilla', 'Boost', ]
+    },
+    {
+        type: 'input',
+        message: 'Please enter your Github username:',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'Are there any questions that need to be addressed?',
+        name: 'questions',
+    },
+
+    {
+        type: 'input',
+        name: 'repo',
+        message: 'What is the URL of the github repo?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+    },
+    {
+        type: 'input',
+        name: 'author',
+        message: 'What is your name?',
     }
 
 ];
@@ -36,17 +88,24 @@ function init() {
       .split(' ')
       .join('')}.json`;
 
-      writeToFile(filename, data);
+            writeToFile(filename, data);
         });
 
 };
 
 // TODO: Create a function to write README file
+// function writeToFile(filename, data) {
+//     fs.writeFile(filename, JSON.stringify(data, null, '\t'), err =>
+//         err ? console.log(err) : console.log('Success!')
+//     );
+// }
+
 function writeToFile(filename, data) {
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), err =>
+    fs.writeFile(`${response.title}.md`, generateMarkdown(response), err =>
         err ? console.log(err) : console.log('Success!')
     );
 }
+
 
 // Function call to initialize app
 init();
