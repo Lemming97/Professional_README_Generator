373a49506478b6a901dec38f803ac9f8490dd50f
Lindsey Martin <martinlindsey87@yahoo.com>
2022-05-27 00:01:11 -0700

    fixed renderLicenseBadge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -22,7 +22,7 @@ function renderLicenseBadge(license) {
   } else if (license === 'Apache') {
     return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
   } else if (license === 'None') {
-    return `No License`
+    return ` `
   }
 
 }
