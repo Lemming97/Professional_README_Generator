76070cce386c0200e3bbbaf1f0278bb9574743d0
Lindsey Martin <martinlindsey87@yahoo.com>
2022-05-31 23:13:56 -0700

    removed unsued function titles and moved comments

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,15 +1,11 @@
+
+
+
+
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
 function renderLicenseBadge(license) {
   if (license === 'Apache 2.0') {
     return '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)';
@@ -27,6 +23,8 @@ function renderLicenseBadge(license) {
 
 }
 
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
 function renderLicenseLink(badge, license) {
   if (license === 'Apache 2.0') {
     return `[${badge}](https://opensource.org/licenses/Apache-2.0)`;
@@ -43,6 +41,8 @@ function renderLicenseLink(badge, license) {
   }
 }
 
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
 function getLicenseSection(licenseStr) {
   if (licenseStr !== 'None') {
     return `
